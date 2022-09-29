d07a14bcbef88f5328777d4fdde5d9d48615e2a7
Maxim Likhachev <macs0007@mail.ru>
2022-09-29 17:26:36 +0300

    fixed getPictures method for MyPlaces

--- Back/index.js
+++ Back/index.js
@@ -25,7 +25,7 @@ app.listen(port, () => {
 })
 
 app.get('/places', corsMiddleware, (req, res) => {
-  knex('places').join('pictures', 'places.eng', '=', 'pictures.eng')
+  knex('pictures').join('places', 'places.eng', '=', 'pictures.eng')
   .then((places) => {
     return res.status(200).json(places);
   })
