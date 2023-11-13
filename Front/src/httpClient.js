16b89c8ca40a33fff0ec8fdf1f765c15fbd78767
Maxim Likhachev <macs0007@mail.ru>
2023-11-13 14:38:21 +0300

    Изменяет переменные окружения

--- Front/src/httpClient.js
+++ Front/src/httpClient.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { initInterceptor } from "./interceptor";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://marshrutka.su/api",
-  //baseURL: "http://localhost:3000",
+  baseURL: `${process.env.VUE_APP_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
